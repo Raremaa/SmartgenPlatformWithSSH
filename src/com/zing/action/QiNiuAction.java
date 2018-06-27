1215c66a7aa619a9ab7c5215bfad833322bf36cd
masaiqi <masaiqi1996@icloud.com>
2018-06-27 12:11:04 +0800

    20180627

--- src/com/zing/action/QiNiuAction.java
+++ src/com/zing/action/QiNiuAction.java
@@ -2,6 +2,7 @@ package com.zing.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.qiniu.util.Auth;
+import org.apache.struts2.json.annotations.JSON;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
@@ -15,7 +16,8 @@ public class QiNiuAction extends ActionSupport {
     private Map<String,Object> datas = new HashMap<String,Object>(0);
 
 
-    public String xuexi(){
+    @JSON(serialize = false)
+    public String getToken(){
         String accessKey = "tENTHr-AKjugTECRTk5pCeRsbYxkGkaAnsbeZav5";
         String secretKey = "Pcj1-qAzkIBBHsbS0RylxRB5wA-1D7MaSzj2ds4h";
         String bucket = "smartgenplatform"; // 七牛空间名（改为自己的）
